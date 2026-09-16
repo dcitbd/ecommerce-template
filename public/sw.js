f6eaaebed58a8a6505dcaeafdcf6d6918efe2e61
Jainal Abedin <jainal.dcitbd@gmail.com>
2026-09-16 14:08:38 +0300

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,16 @@
+// Techno World BD - Service Worker Cache
+const CACHE_NAME = 'twbd-pwa-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
